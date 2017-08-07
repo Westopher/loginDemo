faeb61ca3738c9c2059abd51309a726617ac5999
West Kraemer <kraemerwest@gmail.com>
2017-08-07 09:46:02 -0400

    first commit

---
+++ logDemo/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  logDemo
+//
+//  Created by West Kraemer on 8/7/17.
+//  Copyright © 2017 West Kraemer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var textField: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var loginInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
